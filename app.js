a8bf1bc175608954b4f6d0abf3700c2c485b9d28
susanmaharjan <530756@nwmissouri.edu>
2020-04-24 16:37:53 -0500

    done

--- app.js
+++ app.js
@@ -6,8 +6,20 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
+const dotenv = require('dotenv');
 
 var app = express();
+dotenv.config({ path: '.env' });
+
+// Set up mongoose connection
+const mongoose = require('mongoose');
+const dev_db_url = process.env.ATLAS_URI;
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true });
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter); 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
